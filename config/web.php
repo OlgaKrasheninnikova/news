39a8b4d5e2c23cfde34a5ef52330fdb4df8bee0e
Olga Krasheninnikova <olga--tru@yandex.ru>
2018-04-01 17:38:08 +0300

    config

---
+++ config/web.php
@@ -0,0 +1,130 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'secret',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'dektrium\user\models\User',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+            'messageConfig' => [
+                'from' => ['admin@website.com' => 'Admin'], // this is needed for sending emails
+                'charset' => 'UTF-8',
+            ]
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'auth' => [
+            'class' => 'auth\Module',
+            'layout' => '//homepage', // Layout when not logged in yet
+            'layoutLogged' => '//main', // Layout for logged in users
+            'attemptsBeforeCaptcha' => 3, // Optional
+            'supportEmail' => 'support@mydomain.com', // Email for notifications
+            'passwordResetTokenExpire' => 3600, // Seconds for token expiration
+            'superAdmins' => ['admin'], // SuperAdmin users
+            'signupWithEmailOnly' => false, // false = signup with username + email, true = only email signup
+            'tableMap' => [ // Optional, but if defined, all must be declared
+                'User' => 'user',
+                'UserStatus' => 'user_status',
+                'ProfileFieldValue' => 'profile_field_value',
+                'ProfileField' => 'profile_field',
+                'ProfileFieldType' => 'profile_field_type',
+            ],
+        ],
+
+        'db' => $db,
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                '/' => 'news/index',
+                'about' => 'site/about',
+                '<id:\d+>' => 'news/view',
+                'admin/users' => 'user/admin',
+            ],
+        ],
+
+        'i18n' => [
+            'translations' => [
+                'someModule.*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                ],
+            ]
+        ],
+
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@dektrium/user/views' => '@app/views/user'
+                ],
+            ],
+        ],
+
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['modules']['user'] = [
+            'class' => 'dektrium\user\Module',
+            'modelMap' => [
+                'User' => 'app\models\User',
+                'SettingsForm' => 'app\models\SettingsForm',
+            ],
+            'controllerMap' => [
+                'admin' => ['class' => 'app\controllers\user\AdminController', 'layout' => '@app/views/layouts/admin']
+            ],
+    ];
+}
+
+return $config;
